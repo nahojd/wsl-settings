4e12b83c167d132f7eca2661086236d3f7f25f04
Johan Driessen <johan@driessen.se>
2019-04-03 21:41:27 +0200

    Added manual install of zsh-syntax-highlighting plugin, as it is not installed automatically with oh-my-zsh anymore

--- setup.sh
+++ setup.sh
@@ -7,6 +7,11 @@ if [ ! -d "$DESTPATH/.oh-my-zsh" ]; then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
+#add zsh-syntax-highlighting maybe?
+if [ ! -d "$DESTPATH/.oh-my-zsh/plugins/zsh-syntax-highlighting" ]; then
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $DESTPATH/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+fi
+
 #copy the stuff
 cp -rvi files/. $DESTPATH
 
