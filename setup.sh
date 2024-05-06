78c9d64e86b937ccafb7dc5509e2d5a99d7cad28
Johan Driessen <jd@approach.se>
2024-05-06 15:52:38 +0200

    Småfix

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ DESTPATH=~
 
 #install oh-my-zsh maybe?
 if [ ! -d "$DESTPATH/.oh-my-zsh" ]; then
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
 #install or update starship
@@ -12,10 +12,8 @@ sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
 #add zsh-syntax-highlighting maybe?
 if [ ! -d "$DESTPATH/.oh-my-zsh/plugins/zsh-syntax-highlighting" ]; then
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $DESTPATH/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $DESTPATH/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 fi
-
-
 
 #copy the stuff
 cp -rvi files/. $DESTPATH
